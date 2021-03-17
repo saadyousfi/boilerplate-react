23eacbd8dc9710eab790b0899c68ba32886fd948
Saâd Yousfi <sudo.yousfi@gmail.com>
2021-03-17 16:41:19 +0100

    add Status type

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,2 +1,11 @@
-export {};
+export enum Status {
+  idle = 'idle',
+  loading = 'loading',
+  fulfilled = 'fulfilled',
+  error = 'error',
+}
 // >>> [NEW ENTITY TYPES BELOW]
+export type Author = { id: string };
+export type AuthorCreator = Omit<Author, 'id'>;
+export type Book = { id: string };
+export type BookCreator = Omit<Book, 'id'>;
